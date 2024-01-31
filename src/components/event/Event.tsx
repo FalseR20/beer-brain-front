edfd489e5d647f933975b01ac809a7bf9024e3c5
FalseR20 <kno3.2002@gmail.com>
2024-01-31 15:00:50 +0300

    feat: add deposits to event page

--- src/components/event/Event.tsx
+++ src/components/event/Event.tsx
@@ -5,8 +5,11 @@ import NotFound from "../NotFound.tsx";
 import "../../css/Event.css";
 import {IDetailedEvent} from "../../interfaces.ts";
 import {catchUnauthorized, FetchError, getDetailedEvent} from "../../fetches.tsx";
-import {Badge, Card, Col, ListGroup, Row, Tab} from "react-bootstrap";
+import {Badge, Button, Card, Col, ListGroup, Row} from "react-bootstrap";
 import {UserAvatar} from "../user/UserAvatar.tsx";
+import {BsGear} from "react-icons/bs";
+import UrlPattern from "url-pattern";
+import {UrlsFront} from "../../urls.ts";
 
 export default function Event() {
   const [event, setEvent] = useState<IDetailedEvent>();
@@ -29,113 +32,134 @@ export default function Event() {
   }
 
   event.users.sort((a, b) => a.balance - b.balance)
+  event.deposits.sort((a, b) => a.payedAt.getTimezoneOffset() - b.payedAt.getTimezoneOffset())
   const balanceFormat = new Intl.NumberFormat("en-us", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
     signDisplay: "exceptZero",
   })
+  const bankFormat = new Intl.NumberFormat("en-us", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+
 
   return (<Template>
     {/* Event header */}
-    <Card>
-      <Card.Body>
-        <Card.Title>
-          {event.name}
-        </Card.Title>
-        <Card.Subtitle className={"text-muted"}>
-          {event.description}
-        </Card.Subtitle>
-      </Card.Body>
-    </Card>
+    <div className={"d-flex flex-row gap-3"}>
+      <Card className={"flex-grow-1"}>
+        <Card.Body className={"d-flex justify-content-between align-items-center px-3 py-2"}>
+          <div>
+            <Card.Title>
+              {event.name}
+            </Card.Title>
+            <Card.Subtitle className={"text-muted"}>
+              {event.description}
+            </Card.Subtitle>
+          </div>
+          <div>
+            <Card.Title className={"text-end"}>
+              <span className={"fs-5 text-muted"}>Bank </span>
+              <span className={"fs-2"}>{bankFormat.format(event.bank)}</span>
+            </Card.Title>
+            <Card.Subtitle className={"text-muted text-end"}>
+              {event.date.toDateString()}
+            </Card.Subtitle>
+          </div>
 
-    <Tab.Container id="users-actions" defaultActiveKey={'#' + event.users[0].username}>
-      <Row className={"m-0 my-3 gap-3"}>
-        {/* Members card */}
-        <Col className={"p-0"}>
-          <Card>
-            <Card.Header>
-              Members
-            </Card.Header>
-            <ListGroup variant={"flush"}>
-              {event.users.map((user) => (
-                <ListGroup.Item key={user.username} action={true} className={"p-3"}
-                                href={`#${user.username}`}>
-                  <div className={"d-flex flex-row gap-3 align-items-center"}>
-                    <div>
-                      <UserAvatar user={user} round={true} size={"4rem"}/>
-                      {user.deposits.length > 0 || user.repayments.length > 0 ? (<>
-                        <Badge bg={"secondary"} pill style={{position: "absolute", left: "4rem"}}>
-                          {user.deposits.length + user.repayments.length}
-                        </Badge>
-                      </>) : ""}
-                    </div>
-                    <div className={"d-flex flex-column justify-content-center"}>
-                      <Card.Title className={"mb-0"}
-                                  style={event.host.username == user.username ? {color: "goldenrod"} : {}}>
-                        {`@${user.username}`}
-                      </Card.Title>
-                      {user.fullName == "" ? "" : <>
-                        <Card.Subtitle
-                          className={"text-muted mt-1"}>{user.fullName}</Card.Subtitle>
-                      </>}
-                    </div>
-                    <div className={"flex-grow-1"}/>
-                    <div className={"d-flex flex-column justify-content-center align-items-end"}>
-                      <h3 className={`mb-0 ${user.balance < 0 ? "text-danger" : ""}`}>
-                        {balanceFormat.format(user.balance)}
-                      </h3>
-                    </div>
+        </Card.Body>
+      </Card>
+      <Button variant={"outline-secondary"}
+              className={"d-flex justify-content-center align-items-center"}
+      >
+        <BsGear size={"2.5rem"}/>
+      </Button>
+    </div>
+
+
+    <Row className={"m-0 my-3 gap-3"}>
+      {/* Members card */}
+      <Col className={"p-0"}>
+        <Card>
+          <Card.Header>
+            Members
+          </Card.Header>
+          <ListGroup variant={"flush"}>
+            {event.users.map((user) => (
+              <ListGroup.Item key={user.username} action={true} className={"p-3"}
+                              href={new UrlPattern(UrlsFront.EVENT_ACTION).stringify({
+                                "eventId": event.id,
+                                "username": user.username
+                              })}>
+                <div className={"d-flex flex-row gap-3 align-items-center"}>
+                  <div>
+                    <UserAvatar user={user} round={true} size={"4rem"}/>
+                    {user.deposits.length > 0 || user.repayments.length > 0 ? (<>
+                      <Badge bg={"secondary"} pill style={{position: "absolute", left: "4rem"}}>
+                        {user.deposits.length + user.repayments.length}
+                      </Badge>
+                    </>) : ""}
                   </div>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Card>
-        </Col>
-        {/* Actions card */}
-        <Col className={"p-0"}>
-          <Card>
-            <Card.Header>
-              Actions
-            </Card.Header>
-            <Tab.Content>
-              {event.users.map(user => (
-                <Tab.Pane eventKey={`#${user.username}`}>
-                  <ListGroup variant={"flush"}>
-                    {user.deposits.length == 0 && user.repayments.length == 0 ? (
-                      <ListGroup.Item>
-                        <div className={"d-flex flex-row gap-3 align-items-center my-1"}>
-                          <Card.Title className={"mb-0"}>
-                            <i>No actions</i>
-                          </Card.Title>
-                        </div>
-                      </ListGroup.Item>
-                    ) : ""}
-                    {user.deposits.map(deposit => (
-                      <ListGroup.Item>
-                        <div className={"d-flex flex-row gap-3 align-items-center my-1"}>
-                          <Card.Title className={"mb-0"}>
-                            {deposit.description == "" ? <i>undefined</i> : deposit.description}
-                          </Card.Title>
-                        </div>
-                      </ListGroup.Item>
-                    ))}
-                    {user.repayments.map(repayment => (
-                      <ListGroup.Item>
-                        <div className={"d-flex flex-row gap-3 align-items-center my-1"}>
-                          <Card.Title className={"mb-0"}>
-                            {repayment.description == "" ? <i>undefined</i> : repayment.description}
-                          </Card.Title>
-                        </div>
-                      </ListGroup.Item>
-                    ))}
-                  </ListGroup>
+                  <div className={"d-flex flex-column justify-content-center"}>
+                    <Card.Title className={"mb-0"}
+                                style={event.host.username == user.username ? {color: "goldenrod"} : {}}>
+                      {`@${user.username}`}
+                    </Card.Title>
+                    {user.fullName == "" ? "" : <>
+                      <Card.Subtitle
+                        className={"text-muted mt-1"}>{user.fullName}</Card.Subtitle>
+                    </>}
+                  </div>
+                  <div className={"flex-grow-1"}/>
+                  <div className={"d-flex flex-column justify-content-center align-items-end"}>
+                    <h3 className={`mb-0 ${user.balance < 0 ? "text-danger" : ""}`}>
+                      {balanceFormat.format(user.balance)}
+                    </h3>
+                  </div>
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Card>
+      </Col>
+      {/* Deposits card */}
+      <Col className={"p-0"}>
+        <Card>
+          <Card.Header>
+            Deposits
+          </Card.Header>
+          <ListGroup variant={"flush"}>
+            {event.deposits.map(deposit => (
+              <ListGroup.Item action={true} key={deposit.id}>
+                <div
+                  className={"d-flex flex-row gap-2 justify-content-between align-items-center"}>
+                  <UserAvatar user={deposit.user} round={true} size={"3rem"}/>
+                  <div className={"d-flex flex-column justify-content-between flex-grow-1"}>
+                    <span className={"fs-5"}>
+                      {deposit.description}
+                    </span>
+                    <span className={"fs-6 text-muted"}>
+                      {deposit.payedAt.toLocaleString()}
+                    </span>
+                  </div>
+                  <span className={"fs-3"}>
+                    {bankFormat.format(deposit.value)}
+                  </span>
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Card>
+        <div className={"d-flex flex-row gap-3 my-3 justify-content-center"}>
+          <Button variant={"primary"}>
+            Add deposit
+          </Button>
+          <Button variant={"success"}>
+            Add repayment
+          </Button>
 
-                </Tab.Pane>
-              ))}
-            </Tab.Content>
-          </Card>
-        </Col>
-      </Row>
-    </Tab.Container>
+        </div>
+      </Col>
+    </Row>
   </Template>);
 }
