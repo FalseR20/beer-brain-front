c870c5c45cb56e4a5c12758a65da1b90cc7f8820
FalseR20 <kno3.2002@gmail.com>
2024-03-19 19:58:32 +0300

    feat: add docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   terserOptions: {
-  //     format: {
-  //       comments: false
-  //     }
-  //   }
-  // }
+  server: {
+    host: true,
+    port: 4173,
+    strictPort: true,
+  }
 })
